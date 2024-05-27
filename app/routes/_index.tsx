5dddc79abed7148757c7c4886fe11f4551ae54cc
Op-Pautu <pautuop@gmail.com>
2024-05-27 14:44:57 +0530

    Build form UI

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { Form, redirect } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -6,6 +7,14 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+
+  const json = Object.fromEntries(formData);
+  console.log(json);
+  return redirect("/");
+}
 
 export default function Index() {
   return (
@@ -15,7 +24,65 @@ export default function Index() {
         Learnings and doings. Updated Weekly.
       </p>
 
-      <div className="mt-4">
+      <div className="my-8 border p-2">
+        <Form method="post">
+          <p className="italic">Create an entry</p>
+          <div className="mt-4">
+            <div>
+              <input type="date" name="date" className="text-gray-700" />
+            </div>
+            <div className="mt-2 space-x-6">
+              <label>
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="category"
+                  value="work"
+                />
+                Work
+              </label>
+              <label>
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="category"
+                  value="learning"
+                />
+                Learning
+              </label>
+              <label>
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="category"
+                  value="interesting-thing"
+                />
+                Interesting Thing
+              </label>
+            </div>
+
+            <div className="mt-2">
+              <textarea
+                name="text"
+                className="w-full text-gray-700"
+                rows={5}
+                placeholder="Write your entry..."
+              />
+            </div>
+
+            <div className="mt-1">
+              <button
+                className="bg-blue-500 text-white font-medium px-4 py-1"
+                type="submit"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </Form>
+      </div>
+
+      <div className="mt-6">
         <p>
           Week of May 27<sup>th</sup>
         </p>
